4c5f3431d4320fe308fbfc7c10c5dd966d56fd1b
ngendah <ngendah@pm.me>
2025-07-21 13:50:34 +0300

    Financial-Assistant: Add an initial Frontend

---
+++ financial-assistant/frontend/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button"
+import { ModeToggle } from '@/components/mode-toggle';
+
+export default function Home() {
+  return (
+    <div>
+      <ModeToggle />
+    </div>
+  );
+}
