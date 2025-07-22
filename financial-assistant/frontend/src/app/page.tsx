13f67bef01f2f537b96f34501cccc0dbcf57743c
ngendah <ngendah@pm.me>
2025-07-22 13:05:51 +0300

    Financial-Assistant: Add theme toggle

--- financial-assistant/frontend/src/app/page.tsx
+++ financial-assistant/frontend/src/app/page.tsx
@@ -2,10 +2,14 @@ import { Prompt } from '@/components/prompt'
 import React from 'react'
 import { ScrollableContainer } from '@/components/scrollable-container'
 import { QueryMessage, ResponseMessage } from '@/components/message'
+import { ModeToggle } from '@/components/mode-toggle'
 
 export default function Home() {
     return (
         <>
+            <nav className="flex flex-row-reverse mt-4 mr-12 ml-12">
+                <ModeToggle />
+            </nav>
             <ScrollableContainer>
                 <QueryMessage message="Hallo today how can i, help you" />
                 <ResponseMessage message="Thank you for your query" />
