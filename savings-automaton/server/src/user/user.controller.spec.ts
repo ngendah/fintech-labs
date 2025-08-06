7b186220acee3269d67ed6f2141ad35924994cfe
ngendah <ngendah@pm.me>
2025-08-06 15:05:44 +0300

    Saving-Automation: Add user-create endpoint and tests

--- savings-automaton/server/src/user/user.controller.spec.ts
+++ savings-automaton/server/src/user/user.controller.spec.ts
@@ -1,18 +1,40 @@
+import * as request from 'supertest';
 import { Test, TestingModule } from '@nestjs/testing';
-import { UserController } from './user.controller';
+import { INestApplication } from '@nestjs/common';
+import { UserModule } from './user.module';
+import { PrismaClient } from '@prisma/client';
+import { mockDeep, DeepMockProxy } from 'jest-mock-extended';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 describe('UserController', () => {
-  let controller: UserController;
+  let app: INestApplication;
+  let prisma: DeepMockProxy<PrismaClient>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [UserController],
-    }).compile();
+      imports: [UserModule],
+    })
+      .overrideProvider(PrismaService)
+      .useValue(mockDeep<PrismaClient>())
+      .compile();
 
-    controller = module.get<UserController>(UserController);
+    prisma = module.get(PrismaService);
+    app = module.createNestApplication();
+    await app.init();
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('/POST user', () => {
+    prisma.user.create.mockResolvedValue({
+      id: 1,
+      name: 'test',
+      email: 'test@email.com',
+      createdAt: Date.now(),
+    });
+
+    request
+      .agent(app.getHttpServer())
+      .post('/user')
+      .send({ name: 'test', email: 'test@email.com' })
+      .expect(201);
   });
 });
--- savings-automaton/server/src/user/user.controller.ts
+++ savings-automaton/server/src/user/user.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Post()
+  async createUser(@Body() data: { name: string; email: string }) {
+    this.userService.create(data);
+  }
+}
