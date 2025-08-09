bd93119c71731e186f4cc886c4dc9b6cb56efaee
ngendah <ngendah@pm.me>
2025-08-09 19:36:58 +0300

    Saving-Automoton: Add a cron-task to prompt-user savings deposit at a predefined frequency

--- savings-automaton/server/src/cron/cron.module.ts
+++ savings-automaton/server/src/cron/cron.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { CronService } from './cron.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { ScheduleModule as NestScheduleModule } from '@nestjs/schedule';
+import { JengaModule } from 'src/partners/jenga/jenga.module';
 
 @Module({
-  imports: [PrismaModule, NestScheduleModule.forRoot()],
+  imports: [PrismaModule, NestScheduleModule.forRoot(), JengaModule],
   providers: [CronService],
 })
 export class CronModule {}
