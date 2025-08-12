183afe78a652346eb72e1739f8e27d1cc99051d8
ngendah <ngendah@pm.me>
2025-08-12 11:49:26 +0300

    Saving-Automaton: Restrict callback endpoint to specific domains

--- savings-automaton/server/src/partners/jenga/jenga.controller.ts
+++ savings-automaton/server/src/partners/jenga/jenga.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Logger, Post } from '@nestjs/common';
 import { DepositConfirmationDto } from './dto/jenga.dto';
 import { JengaService } from './jenga.service';
 
-@Controller({ path: 'daraja', host: '*.finserve.africa' })
+@Controller({ path: 'daraja', host: ':subdomain.finserve.africa' })
 export class JengaController {
   private readonly logger = new Logger(JengaService.name, { timestamp: true });
 
