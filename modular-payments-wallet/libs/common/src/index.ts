cd1f6b27ac14ea644b95f663b20d185856f9ff89
ngendah <ngendah@pm.me>
2025-09-09 17:46:52 +0300

    Modular-Payments-Wallet: Export services names from a common lib

--- modular-payments-wallet/libs/common/src/index.ts
+++ modular-payments-wallet/libs/common/src/index.ts
@@ -1,2 +1,3 @@
-export * from './common.module';
-export * from './common.service';
+export const FRAUD_DETECTION_SERVICE = Symbol("fraud-detection-service")
+export const KYC_SERVICE = Symbol("kyc-service")
+export const NOTIFICATION_SERVICE = Symbol("notification-service")
