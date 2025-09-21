04d51be9d2ed22d2f3c002f6adf8f8905218d9b4
ngendah <ngendah@pm.me>
2025-09-21 10:07:28 +0300

    Modular-Payments-Wallet: Add a health check endpoint

---
+++ modular-payments-wallet/apps/api/src/api.controller.ts
@@ -0,0 +1,12 @@
+import {
+  Controller,
+  Get,
+} from '@nestjs/common';
+
+@Controller()
+export class ApiController {
+  @Get()
+  pong() {
+    return 'pong';
+  }
+}
--- modular-payments-wallet/apps/api/src/api.module.ts
+++ modular-payments-wallet/apps/api/src/api.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CoreModule } from './core/core.module';
+import { ApiController } from './api.controller';
 
 @Module({
   imports: [CoreModule],
+  controllers: [ApiController]
 })
-export class ApiModule {}
+export class ApiModule { }
