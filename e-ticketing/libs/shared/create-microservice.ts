eb05dac3ec4908de0eb21a2127a6559b73c84716
ngendah <ngendah@pm.me>
2025-11-07 19:27:16 +0300

    ETicketing: Minor styling to create-microservices file

--- e-ticketing/libs/shared/create-microservice.ts
+++ e-ticketing/libs/shared/create-microservice.ts
@@ -7,9 +7,16 @@ import {
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { DynamicModule, ForwardReference, Type } from '@nestjs/common';
 
-type IEntryNestModule = Type<any> | DynamicModule | ForwardReference | Promise<IEntryNestModule>;
+type IEntryNestModule =
+  | Type<any>
+  | DynamicModule
+  | ForwardReference
+  | Promise<IEntryNestModule>;
 
-export const createMicroServiceApp = async (module: IEntryNestModule, name: string) =>
+export const createMicroServiceApp = async (
+  module: IEntryNestModule,
+  name: string,
+) =>
   NestFactory.createMicroservice<AsyncMicroserviceOptions>(module, {
     useFactory: (config: ConfigService) => {
       const host = config.get('NATS_HOST', 'localhost');
