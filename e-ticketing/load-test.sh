f1f354f127aa72b199ec378c121c08aa68685334
ngendah <ngendah@pm.me>
2025-11-21 14:39:40 +0300

    ETicketing: improve checks on load-test script

--- e-ticketing/load-test.sh
+++ e-ticketing/load-test.sh
@@ -1,4 +1,10 @@
 sudo docker compose down --volumes && sudo docker compose up -d --build &
+pid=$!
+wait $pid
+if [ $? -ne 0 ]; then
+  echo "Docker command failed"
+  exit 1
+fi
 for i in {1..40}; do
   printf "."
   sleep 1
