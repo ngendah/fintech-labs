4ac35750d281e9bd122889bfc15d7d707be98c08
ngendah <ngendah@pm.me>
2025-11-22 13:13:13 +0300

    ETicketing: set log-level to error

--- e-ticketing/apps/api-gateway/src/main.ts
+++ e-ticketing/apps/api-gateway/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { loggingLevel } from 'libs/shared';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const logLevel = loggingLevel();
+  const app = await NestFactory.create(AppModule, { logger: logLevel });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- e-ticketing/libs/shared/create-microservice.ts
+++ e-ticketing/libs/shared/create-microservice.ts
@@ -5,7 +5,13 @@ import {
   Transport,
 } from '@nestjs/microservices';
 import { ConfigService } from '@nestjs/config';
-import { DynamicModule, ForwardReference, Logger, Type } from '@nestjs/common';
+import {
+  DynamicModule,
+  ForwardReference,
+  Logger,
+  LogLevel,
+  Type,
+} from '@nestjs/common';
 import { NATS_DEFAULT_URI, NATS_URI } from './constants';
 
 type IEntryNestModule =
@@ -14,11 +20,29 @@ type IEntryNestModule =
   | ForwardReference
   | Promise<IEntryNestModule>;
 
+export function loggingLevel(): LogLevel[] {
+  const allowed: LogLevel[] = [
+    'verbose',
+    'debug',
+    'log',
+    'warn',
+    'error',
+    'fatal',
+  ];
+  const level = process.env.LOG_LEVEL;
+  return allowed.includes((level as LogLevel) ?? undefined)
+    ? ([level!] as LogLevel[])
+    : (['verbose'] as LogLevel[]);
+}
+
 export const createMicroServiceApp = async (
   module: IEntryNestModule,
   name: string,
 ) => {
-  const app = await NestFactory.create(module);
+  const logLevel = loggingLevel();
+  const app = await NestFactory.create(module, {
+    logger: logLevel,
+  });
   const logger = new Logger(`createMicroServiceApp(${name})`);
   app.connectMicroservice<AsyncMicroserviceOptions>({
     useFactory: (config: ConfigService) => {
@@ -53,11 +77,13 @@ export const createMicroserviceClientModule = (name: string) => {
     {
       name,
       useFactory: async (config: ConfigService) => {
+        const logLevel = loggingLevel();
         const logger = new Logger(`createMicroserviceClientModule(${name})`);
         const natsUri = config.get<string>(NATS_URI, NATS_DEFAULT_URI);
         const servers = natsUri.split(',');
         logger.debug(`nats-servers: ${servers}`);
         return {
+          logger: logLevel,
           transport: Transport.NATS,
           options: {
             queue: name,
